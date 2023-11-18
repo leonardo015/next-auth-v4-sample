95db6341239f01499663839fec8499410d356b86
Leonardo Sampaio <leonardo.html@gmail.com>
2023-11-18 14:41:34 -0300

    log session from layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,7 @@
 import { getServerSession } from "next-auth";
-import Image from "next/image";
 
 export default async function Home() {
 	const session = await getServerSession();
-	console.log(session);
 
 	return (
 		<>
