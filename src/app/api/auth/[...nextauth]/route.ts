23643008ddf9d47b352fed51fd039fa7f70e92a5
Leonardo Sampaio <leonardo.html@gmail.com>
2023-11-18 00:25:28 -0300

    auth using credentials provider

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,11 +1,45 @@
+import { User } from "@/app/lib/definitions";
+import { sql } from "@vercel/postgres";
+import bcrypt from "bcrypt";
 import NextAuth from "next-auth";
-import GithubProvider from "next-auth/providers/github";
+import CredentialsProvider from "next-auth/providers/credentials";
+import { z } from "zod";
+
+async function getUser(email: string): Promise<User | undefined> {
+	try {
+		const user = await sql<User>`SELECT * from USERS where email=${email}`;
+		return user.rows[0];
+	} catch (error) {
+		console.error("Failed to fetch user:", error);
+		throw new Error("Failed to fetch user.");
+	}
+}
 
 export const authOptions = {
 	providers: [
-		GithubProvider({
-			clientId: process.env.GITHUB_ID ?? "",
-			clientSecret: process.env.GITHUB_SECRET ?? "",
+		CredentialsProvider({
+			name: "Credentials",
+			credentials: {
+				email: { label: "Email", type: "text" },
+				password: { label: "Password", type: "password" },
+			},
+			async authorize(credentials, req) {
+				const parsedCredentials = z
+					.object({ email: z.string().email(), password: z.string().min(6) })
+					.safeParse(credentials);
+
+				if (parsedCredentials.success) {
+					const { email, password } = parsedCredentials.data;
+					const user = await getUser(email);
+					if (!user) return null;
+					const passwordsMatch = await bcrypt.compare(password, user.password);
+
+					if (passwordsMatch) return user;
+				}
+
+				console.log("Invalid credentials");
+				return null;
+			},
 		}),
 	],
 };
